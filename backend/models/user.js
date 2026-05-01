5e2c5c43e60ad937383c0138ddb339e0f5c4ce03
ADKBhagya <adkbhagya13@gmail.com>
2026-05-02 03:22:35 +0530

    feat(user): update user schema with new fields and roles

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,29 +1,39 @@
+// models/User.js
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   fullName: String,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+  email: { type: String, unique: true },
+  password: String,
+
   organization: String,
-  role: String,
-  password: {
+
+  role: {
     type: String,
-    required: true,
+    enum: ["Admin", "Manufacturer", "Retailer", "Auditor", "Authority", "Logistics", "Repair Center", "Recycler" ],
+    default: "Manufacturer"
   },
-  
-    // ✅ ADD THIS
-  isApproved: {
-    type: Boolean,
-    default: false,
-  },
-  
+
+status: {
+  type: String,
+  enum: ["pending", "approved", "rejected"],
+  default: "pending",
+},
+isApproved: {
+  type: Boolean,
+  default: false,
+},
+
+  createdAt: { type: Date, default: Date.now },
+
+  // SECURITY
+  lastLogin: Date,
+  loginAttempts: { type: Number, default: 0 },
+
+  // RESET
   resetToken: String,
-  resetTokenExpiry: Date,
+  resetTokenExpiry: Date
 });
 
-const User = mongoose.model("User", userSchema);
-
-export default User;
+export default mongoose.model("User", userSchema);
