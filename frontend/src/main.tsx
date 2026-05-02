31d44e4e439039f5ebb1d6f2d1981c374b64b5fa
ADKBhagya <adkbhagya13@gmail.com>
2026-05-02 21:59:06 +0530

    feat(frontend): update application entry and provider setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+      <Toaster position="top-right" richColors />
+    </BrowserRouter>
+  </StrictMode>
 );
