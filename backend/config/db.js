662bf7f705fdfe70a0b2d20ea27ee6a7280df8cb
ADKBhagya <adkbhagya13@gmail.com>
2026-05-11 00:10:02 +0530

    fix(backend): improve MongoDB connection configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://admin:12345678_admin@ac-owaq0qj-shard-00-00.xb6nriq.mongodb.net:27017,ac-owaq0qj-shard-00-01.xb6nriq.mongodb.net:27017,ac-owaq0qj-shard-00-02.xb6nriq.mongodb.net:27017/?ssl=true&replicaSet=atlas-tqqhok-shard-0&authSource=admin&appName=loopi-cluster");
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected ✅");
   } catch (error) {
